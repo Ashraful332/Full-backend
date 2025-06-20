a3de24a645ceca025ed31940eef07ba5bc055ece
Ashraful Pathan <ashrafulhasan7116@gmail.com>
2025-06-20 11:55:50 +0600

    🥹🥹🥹🥹

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['example.com'],
+  },
+  // আরও কনফিগ এখানে যোগ করতে পারো
+};
+
+module.exports = nextConfig;
